5dabd0ea41754cba2f4e7a8e90c3e076183cc554
Mario Garcia <mariogarcia.ar@gmail.com>
2022-02-08 19:19:08 -0300

    logins works

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,31 @@
+import React, { useState } from 'react';
 import './App.css';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
+import Login from '../Login/Login';
 
-function App() {
+
+import { useToken } from '../../services/auth';
+
+function App() { 
+  const { token, setToken } = useToken();
+
+  // If not logued return login form
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
-    <div className="App">
-      Union Tracking
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/preferences' element={<Preferences />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/services/auth.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+export async function loginUser(credentials) {
+    return fetch('https://uniontracking-3.frogi.dev/api/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+}
+ 
+
+export  function useToken() {
+    const getToken = () => {
+        const tokenString = localStorage.getItem('token');
+        const userToken = JSON.parse(tokenString);
+        return userToken?.data.token
+    };
+
+    const [token, setToken] = useState(getToken());
+
+    const saveToken = userToken => {
+        localStorage.setItem('token', JSON.stringify(userToken));
+        setToken(userToken.data.token);
+    };
+
+    return {
+        setToken: saveToken,
+        token
+    }
+}
